b989c23ce7e76dc6e3b5b02d76362d3b1af665ef
Diamond <2210054954@qq.com>
2025-12-18 14:33:17 +0800

    config(vite.config): 更新服务器配置以支持IPv6和指定允许的Host

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,10 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
+    // 监听所有 IPv6 地址
     host: "::",
+    // 监听所有 IPv4 地址
+    // host: "0.0.0.0",
     port: 5173,
     proxy: {
       '/api': {
@@ -13,7 +16,11 @@ export default defineConfig({
         changeOrigin: true,
         secure: false,
       }
-    }
+    },
+    // 允许通过这些 Host 访问 用于内网穿透
+    allowedHosts: [
+      'd1amond.cn',
+    ],
   },
   plugins: [react()],
   resolve: {
